d48179a2019e66a70987d696f47fff4152d4233e
IsabellaBu <mariaisabelabu@hotmail.com>
2022-08-03 22:26:14 -0500

    pagina angular comida

---
+++ src/app/services/order-details.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderDetailsService {
+
+  constructor() { }
+
+  // fooddetails 
+  
+  foodDetails = [
+    {
+      id:1,
+      foodName:"Sandwich",
+      foodDetails:"Sandwich de pollo y queso.",
+      foodPrice:20000,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wzfq7djolqxgdhghebbq"
+    },
+    {
+      id:2,
+      foodName:"Pizza Vegetariana",
+      foodDetails:"Pizza de aceitunas, con vegetales",
+      foodPrice:36900,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/sgbobtbxlojbtdnr2m5k"
+    },
+    {
+      id:3,
+      foodName:"Hmaburguesa de Pollo",
+      foodDetails:"Hamburguesa de pollo apanado con salsa curry",
+      foodPrice:14900,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/xbeqlsck3p0kei53to7k"
+    },
+    {
+      id:4,
+      foodName:"Burrito de Pollo",
+      foodDetails:"Burrito de Pollo al mejor estilo Mexicano",
+      foodPrice:14000,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/l2ng6gtge30sqaafqng7"
+    },
+    {
+      id:5,
+      foodName:"Brownie",
+      foodDetails:" Delicioso brownie de chocolate",
+      foodPrice:10500,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/iqlmbg1hlyc0dspdyzzv"
+    },
+    {
+      id:6,
+      foodName:"Helado de Oreo Cheesecake",
+      foodDetails:"Helado de oreo con chocolate",
+      foodPrice:21900,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wtj8esaeslvlscv8glj6"
+    }
+  ]
+
+
+  
+
+}
